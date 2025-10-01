9d8b72a808cade50ec708c96ed626b5cd81ee603
Jonathan Yang <yangjonathan9@gmail.com>
2025-10-01 09:57:03 -0700

    jwt token authentication

---
+++ src/middlewares/authentication.js
@@ -0,0 +1,18 @@
+import 'dotenv/config'
+import jwt from 'jsonwebtoken'
+
+// Will add userId to the request on successful token verification
+const authenticateToken = (req, res, next) => {
+    const authHeader = req.headers['authorization']
+    const token = authHeader && authHeader.split(' ')[1]
+
+    if (!token) return res.status(401).json({ message: "Token required" })
+
+    jwt.verify(token, process.env.JWT_SECRET, (err, decoded) => {
+        if (err) return res.status(403).json({ message: "Invalid or expired token" })
+        req.userId = decoded.userId
+        next()
+    })
+}
+
+export default authenticateToken
--- src/routes/auth.js
+++ src/routes/auth.js
@@ -22,7 +22,7 @@ router.post('/signup', signupValidator, async (req, res) => {
             email: email,
             password: hashedPassword
         })
-        const token = jwt.sign({id: newUser._id}, process.env.JWT_SECRET, {expiresIn: '1h'})
+        const token = jwt.sign({userId: newUser._id}, process.env.JWT_SECRET, {expiresIn: '1h'})
         res.status(201).json({ message: 'User created!', token: token });
     } catch (err) {
         res.status(500).json({ message: 'Server error' })
@@ -39,7 +39,7 @@ router.post('/login', loginValidator, async (req, res) => {
         const passwordMatch = await bcrypt.compare(password, user.password)
         if (!passwordMatch) return res.status(401).json({ message: 'Incorrect password!' })
 
-        const token = jwt.sign({id: user._id}, process.env.JWT_SECRET, {expiresIn: '1h'})
+        const token = jwt.sign({userId: user._id}, process.env.JWT_SECRET, {expiresIn: '1h'})
         res.status(200).json({ message: 'Successfully logged in!', token: token });
     } catch (err) {
         res.status(500).json({ message: 'Server error' })
