8838cfaa94efb17c13040bc5d72e5c0b038e69f3
Jonathan Yang <yangjonathan9@gmail.com>
2025-10-02 14:44:45 -0700

    added patch routes

--- src/routes/user.js
+++ src/routes/user.js
@@ -53,16 +53,76 @@ router.post('/jobs', authenticateToken, jobValidator, async (req, res) => {
 
 router.patch('/projects/:id', authenticateToken, async (req, res) => {
     const projectId = req.params.id
+    const userId = req.userId
     const updates = req.body
+
+    const allowedUpdates = ['name', 'status', 'description', 'langs', 'tasks', 'githubLink'];
+    const filteredUpdates = {};
+    for (const key of allowedUpdates) {
+        if (req.body[key] !== undefined) filteredUpdates[key] = req.body[key];
+}
     try {
-        const updatedProject = await Project.findByIdAndUpdate(projectId, updates, {
-            new: true,
-            runValidators: true
+        const updatedProject = await Project.findOneAndUpdate(
+            {
+                _id: projectId,
+                user: userId
+            }, 
+            filteredUpdates, 
+            {
+                new: true,
+                runValidators: true
         })
 
+        if (!updatedProject) 
+            return res.status(404).json({ message: 'Project not found' })
+        res.status(200).json(updatedProject)
     } catch (err) {
-
+        res.status(400).json({ message: 'Failed to update project' })
     }
 })
+
+router.patch('/jobs/:id', authenticateToken, async (req, res) => {
+    const jobId = req.params.id
+    const userId = req.userId
+    const updates = req.body
+
+    const allowedUpdates = [
+        'company',
+        'status',
+        'position',
+        'location',
+        'startDate',
+        'duration',
+        'deadline',
+        'dateApplied',
+        'interviewDate',
+        'applicationLink'
+    ];
+    const filteredUpdates = {};
+    for (const key of allowedUpdates) {
+        if (updates[key] !== undefined) filteredUpdates[key] = updates[key];
+    }
+
+    try {
+        const updatedJob = await Job.findOneAndUpdate(
+            {
+                _id: jobId,
+                user: userId
+            }, 
+            filteredUpdates, 
+            {
+                new: true,
+                runValidators: true
+        })
+
+        if (!updatedJob) 
+            return res.status(404).json({ message: 'Job not found' })
+        res.status(200).json(updatedJob)
+    } catch (err) {
+        res.status(400).json({ message: 'Failed to update job' })
+    }
+})
+
+
 
 export default router
